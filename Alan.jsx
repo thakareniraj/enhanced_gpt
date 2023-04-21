bbb50e325d535c78487ad2fcd6052abe5b29cf09
thakareniraj <96050843+thakareniraj@users.noreply.github.com>
2023-04-21 16:17:55 +0530

    commit

--- Alan.jsx
+++ Alan.jsx
@@ -6,7 +6,7 @@ const Alan = () => {
   const [name, setName] = useState('');
   const [phone, setPhone] = useState('')
   const [address, setAddress] = useState('');
-  const ALAN_Key = `aae908c68a98d7132e5c4641906dafb32e956eca572e1d8b807a3e2338fdd0dc/stage`
+  const ALAN_Key = ``
   useEffect(() => {
     alanBtn({
         key: ALAN_Key,
